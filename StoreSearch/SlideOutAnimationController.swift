991acfda0c94551786d9b939b9f1990f975887c7
jzhou <jzhou23@stevens.edu>
2017-12-01 17:20:00 -0500

    add gradient background and animations for detail view controller

---
+++ StoreSearch/SlideOutAnimationController.swift
@@ -0,0 +1,31 @@
+//
+//  SlideOutAnimationController.swift
+//  StoreSearch
+//
+//  Created by Jiahuang Zhou on 12/1/17.
+//  Copyright © 2017 jhzhou. All rights reserved.
+//
+
+import UIKit
+
+class SlideOutAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        if let fromView = transitionContext.view(forKey: .from) {
+            let containerView = transitionContext.containerView
+            let time = transitionDuration(using: transitionContext)
+            
+            UIView.animate(withDuration: time, animations: {
+                fromView.center.y -= containerView.bounds.size.height
+                fromView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5  )
+            }, completion: { finished in
+                transitionContext.completeTransition(finished)
+            })
+        }
+    }
+    
+}
